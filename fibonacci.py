0cc4bca1df4fe68c9bc6238a7f00fa1ea5b8a030
rav <115064706+ravabasya06@users.noreply.github.com>
2022-10-05 14:13:14 +0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,27 @@
-input_angka = int(input("masukkan batas angka : "))
-
-angka1 = 0
-angka2 = 1
-fib_arr = []
-angka_terakhir = angka1 + angka2
-
-while angka1 < input_angka:
-    fib_arr.append(angka1)
-    angka_terakhir = angka1 + angka2
-    angka1 = angka2
-    angka2 = angka_terakhir
+def ulang():
     
-print(fib_arr)
+    input_angka = int(input("masukkan batas angka : "))
+    
+    angka1 = 0
+    angka2 = 1
+    fib_arr = []
+    angka_terakhir = angka1 + angka2
+    
+    while angka1 < input_angka:
+        fib_arr.append(angka1)
+        angka_terakhir = angka1 + angka2
+        angka1 = angka2
+        angka2 = angka_terakhir
+        
+    print(fib_arr)
+    
+ulang()
+
+pengulangan = input("Lakukan perhitungan lagi? ya / tidak : ")
+
+if pengulangan == "ya":
+    ulang()
+elif pengulangan == "tidak":
+    print("ok")
+else:
+    print("Saya tidak mengerti perintah tersebut, saya akan mengira anda menjawab tidak.")
