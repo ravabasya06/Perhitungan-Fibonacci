f93fbd8e0ea3ba668c2032d80ff54202d3c6f2f4
rav <115064706+ravabasya06@users.noreply.github.com>
2022-10-11 14:36:57 +0700

    Update fibonacci.py
    fix function ulang

--- fibonacci.py
+++ fibonacci.py
@@ -6,21 +6,18 @@ def ulang():
     angka2 = 1
     fib_arr = []
     
-    while angka1 < input_angka:
+    while angka1 <= input_angka:
         fib_arr.append(angka1)
         angka_terakhir = angka1 + angka2
         angka1 = angka2
         angka2 = angka_terakhir
         
     print(fib_arr)
-    
+    pengulangan = input("Lakukan perhitungan lagi? ya / tidak : ")
+    if pengulangan == "ya":
+        ulang()
+    elif pengulangan == "tidak":
+        print("ok")
+    else:
+        print("Saya tidak mengerti perintah tersebut, saya akan mengira anda menjawab tidak.")
 ulang()
-
-pengulangan = input("Lakukan perhitungan lagi? ya / tidak : ")
-
-if pengulangan == "ya":
-    ulang()
-elif pengulangan == "tidak":
-    print("ok")
-else:
-    print("Saya tidak mengerti perintah tersebut, saya akan mengira anda menjawab tidak.")
